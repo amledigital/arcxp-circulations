202454eb319077e3d2ba2b7806423cbf9badba56
Andrew M McCall <andrew@ledigital.com>
2024-10-07 15:57:15 -0400

    almost done with grabbing section articles by section id

--- internal/handlers/handle_get_arc_section.go
+++ internal/handlers/handle_get_arc_section.go
@@ -1,18 +1,102 @@
 package handlers
 
-import "github.com/gin-gonic/gin"
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+
+	"github.com/amledigital/arcxp-circulations/utils/httpclient"
+	"github.com/gin-gonic/gin"
+)
+
+type Nested struct {
+	Path  string `json:"path"`
+	Query Query  `json:"query"`
+}
+
+type Must struct {
+	Term  map[string]any `json:"term,omitempty"`
+	Terms map[string]any `json:"terms,omitempty"`
+}
+
+type MultiMust struct {
+	Terms map[string]any `json:"terms,omitempty"`
+}
+
+type Bool struct {
+	Must []map[string]any `json:"must,omitempty"`
+}
+
+type Query struct {
+	Bool Bool `json:"bool"`
+}
+
+type ElasticQuery struct {
+	Query `json:"query"`
+}
 
 func (hr *HandlerRepo) HandleGetArcSection(c *gin.Context) {
 
-	site := c.Params.ByName("arcWebsite")
-
 	sectionID := c.Query("sectionID")
 
-	type payload struct {
-		Website string `json:"website"`
-		Section string `json:"section_id"`
+	website := c.Param("website")
+
+	if website == "" {
+		website = hr.App.ArcWebsite
 	}
 
-	c.JSON(200, payload{Website: site, Section: sectionID})
+	var q ElasticQuery
+
+	revisionTerm := make(map[string]any)
+
+	revisionTerm["revision.published"] = true
+
+	q.Query.Bool.Must = append(q.Query.Bool.Must, map[string]interface{}{"term": revisionTerm})
+
+	typeTerm := make(map[string]any)
+
+	typeTerm["type"] = "story"
+
+	q.Query.Bool.Must = append(q.Query.Bool.Must, map[string]interface{}{"term": typeTerm})
+
+	var ns Nested
+
+	ns.Path = "taxonomy.sections"
+
+	ns.Query.Bool.Must = []map[string]any{}
+
+	sectionTerm := make(map[string]any)
+
+	sectionTerm["terms"] = map[string]any{
+		"taxonomy.sections._id": []string{sectionID},
+	}
+
+	ns.Query.Bool.Must = append(ns.Query.Bool.Must, map[string]interface{}{"terms": sectionTerm})
+
+	nsSectionWebsite := make(map[string]any)
+
+	nsSectionWebsite["taxonomy.sections._website"] = website
+
+	ns.Query.Bool.Must = append(ns.Query.Bool.Must, map[string]interface{}{"term": nsSectionWebsite})
+
+	q.Query.Bool.Must = append(q.Query.Bool.Must, map[string]interface{}{"nested": ns})
+
+	out, err := json.Marshal(q)
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	client := httpclient.NewHttpClient("GET", fmt.Sprintf("%s/content/v4/search?website=%s&body=%s",
+		hr.App.ArcContentBase,
+		hr.App.ArcWebsite, out), hr.App.ArcAccessToken, nil)
+
+	articles, next, err := client.FetchArticlesBySectionID(sectionID)
+
+	c.JSON(200, map[string]interface{}{
+		"articles": articles,
+		"next":     next,
+		"error":    err,
+	})
 
 }
--- utils/httpclient/httpclient.go
+++ utils/httpclient/httpclient.go
@@ -3,6 +3,7 @@ package httpclient
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
 	"io"
 	"log"
 	"net/http"
@@ -24,6 +25,56 @@ func NewHttpClient(method, url, token string, data []byte) *HttpClient {
 		Body:      data,
 		AuthToken: token,
 	}
+}
+
+func (h *HttpClient) FetchArticlesBySectionID(sectionID string) (contentElements []interface{}, more int, err error) {
+
+	fmt.Println(h.URL)
+
+	client := &http.Client{}
+
+	req, err := http.NewRequest(h.Method, h.URL, bytes.NewReader(h.Body))
+
+	if err != nil {
+		return nil, 0, err
+	}
+
+	req.Header.Add("Authorization", "Bearer "+h.AuthToken)
+	req.Header.Set("Content-Type", "application/json")
+
+	resp, err := client.Do(req)
+
+	if err != nil {
+		return nil, 0, err
+	}
+
+	if resp.Body != nil {
+		defer resp.Body.Close()
+
+		body, err := io.ReadAll(resp.Body)
+
+		if err != nil {
+			log.Fatalln(err)
+			return nil, 0, err
+		}
+
+		var c = make(map[string]any)
+
+		err = json.Unmarshal(body, &c)
+
+		if err != nil {
+			log.Fatalln(err)
+			return nil, 0, err
+		}
+
+		fmt.Println(c)
+
+		return nil, 0, nil
+
+	}
+
+	return nil, 0, nil
+
 }
 
 func (h *HttpClient) FetchCirculationsByID(documentID, website string) (*models.Circulations, error) {
